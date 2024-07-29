fc89b2a2c69f59283de325bbccb66d2420ab91d6
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-07-29 15:56:47 +0200

    Added routing to App.svelte and updated ProductCard.svelte to navigate to product detail page

--- E-Commerce-Store/src/App.svelte
+++ E-Commerce-Store/src/App.svelte
@@ -1,7 +1,13 @@
 <script>
+  import ProductDetailView from "./components/product/ProductDetailView.svelte";
   import Header from "./components/Header.svelte";
   import Home from "./pages/Home.svelte";
+  import { Route, Router } from "svelte-routing";
 </script>
 
-<Header/>
-<Home/>
+
+<Router >
+  <Header/>
+  <Route path="/" component={Home}/>
+  <Route path="/:id" component={ProductDetailView}/>
+</Router>
