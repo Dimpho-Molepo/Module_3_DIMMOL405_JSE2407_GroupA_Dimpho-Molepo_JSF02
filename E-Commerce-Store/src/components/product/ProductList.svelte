e4955fea7877dc7432cbb23581980ada5cbd1193
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-07-30 07:43:29 +0200

    Update the product list to use the ProductStore.js functions

--- E-Commerce-Store/src/components/product/ProductList.svelte
+++ E-Commerce-Store/src/components/product/ProductList.svelte
@@ -2,27 +2,25 @@
   import { onMount } from 'svelte';
   import ProductCard from "./ProductCard.svelte";
   import CardSkeleton from "./CardSkeleton.svelte";
-  // import Error from "../Error.svelte";
   import { productStore, filteredSortedProducts } from "../../store/ProductStore";
 
   onMount(() => {
     productStore.fetchProducts();
+    productStore.fetchCategories();
   });
-
-  $: ({ loading, error } = $productStore);
 </script>
 
-{#if loading && !error}
+{#if $productStore.loading}
   <div class="grid justify-center">
     <div class="lg:max-h-[130rem] max-w-xl mx-auto grid gap-4 grid-cols-1 lg:grid-cols-4 md:grid-cols-2 items-center lg:max-w-none my-4">
-      {#each Array(20) as _, index}
-        <CardSkeleton {index} />
+      {#each Array(20) as _}
+        <CardSkeleton />
       {/each}
     </div>
   </div>
-{:else if error}
+{:else if $productStore.error}
   <div class="grid justify-center">
-    <!-- <Error {...error} /> -->
+    <p>Error: {$productStore.error}</p>
   </div>
 {:else}
   <div class="grid justify-center">
